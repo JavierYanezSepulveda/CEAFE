b1ecfb09c18edf72bc3199d9c35904531b1c5b79
JavierYanezSepulveda <javierandres.y.s@gmail.com>
2019-11-04 11:11:04 -0300

    prueba1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Lolavel
                 </div>
 
                 <div class="links">
